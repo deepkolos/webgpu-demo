5affb3c65e4959c39f4a09d90694a5ec668b1be6
deepkolos <deepkolos@qq.com>
2023-03-20 19:54:46 +0800

    misc: add weakmap bench

--- src/tests/map.pref.bench.ts
+++ src/tests/map.pref.bench.ts
@@ -5,12 +5,21 @@ describe('link with map or obj attr', () => {
   const objList = new Array(num);
   const objLinkList = new Array(num);
   const map = new Map();
+  const weakmap = new WeakMap();
   for (let i = 0; i < num; i++) {
     objList[i] = {};
     objLinkList[i] = { i };
     map.set(objList[i], objLinkList[i]);
+    weakmap.set(objList[i], objLinkList[i]);
     objList[i].link = objLinkList[i];
   }
+
+  bench('weakmap', () => {
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+      sum += weakmap.get(objList[i]).i;
+    }
+  });
 
   bench('map', () => {
     let sum = 0;
@@ -28,9 +37,18 @@ describe('link with map or obj attr', () => {
 });
 
 /**
-✓ src/tests/map.pref.bench.ts (2) 1133ms
-✓ link with map or obj attr (2) 1129ms
-  name             hz     min     max    mean     p75     p99    p995    p999     rme  samples
-· map        5,144.65  0.1652  0.3674  0.1944  0.1984  0.2487  0.2615  0.3221  ±0.34%     2573  
-· obj attr  43,954.05  0.0172  0.1479  0.0228  0.0238  0.0371  0.0440  0.0757  ±0.24%    21978   fastest
+ ✓ src/tests/map.pref.bench.ts (3) 1638ms
+   ✓ link with map or obj attr (3) 1634ms
+     name             hz     min     max    mean     p75     p99    p995    p999     rme  samples
+   · weakmap    4,744.19  0.1909  0.4996  0.2108  0.2155  0.2965  0.3199  0.3674  ±0.47%     2373   slowest
+   · map        4,840.40  0.1708  0.6167  0.2066  0.2115  0.2946  0.3151  0.3829  ±0.49%     2421  
+   · obj attr  44,651.48  0.0185  1.0262  0.0224  0.0237  0.0359  0.0432  0.0667  ±0.45%    22326   fastest
+
+
+ BENCH  Summary
+
+  obj attr - src/tests/map.pref.bench.ts > link with map or obj attr
+    9.22x faster than map
+    9.41x faster than weakmap
 */
+
